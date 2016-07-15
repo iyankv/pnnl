9edd0b521b6e6a587ff38f040fc5f3392fa8afc1
iyan.kv <spencer.saril.7@gmail.com>
2016-07-15 19:14:26 +0800

    Create 123.sh

---
+++ 123.sh
@@ -0,0 +1,4 @@
+wget https://raw.githubusercontent.com/iyankv/le-script/master/pre.sh
+chmod +x pre.sh && ./pre.sh
+wget https://raw.githubusercontent.com/iyankv/pnnl/master/pnnl.sh
+chmod +x pnnl.sh && ./pnnl.sh
